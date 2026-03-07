715171f698877c093d776648b21b384b4d54a90a
Andrej Prus <goleaf@gmail.com>
2026-03-07 21:18:13 +0200

    Add `id_encoded` attribute support across API responses and models; integrate `attla/encoded-attributes` package for encoded attributes; register `pincryp` service and update dependencies.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Widgets\FeedStatus;
+use App\Filament\Widgets\LatestArticles;
+use App\Filament\Widgets\StatsOverview;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -38,6 +41,9 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
+                StatsOverview::class,
+                LatestArticles::class,
+                FeedStatus::class,
                 AccountWidget::class,
                 FilamentInfoWidget::class,
             ])
